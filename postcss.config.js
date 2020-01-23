14bb73a111b19d56fda98124039217931d9172e8
Ryan Scherler <ryan@eastslope.studio>
2020-01-22 22:37:42 -0800

    Remove autoprefixer as its already included in PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -11,7 +11,6 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('postcss-nested'),
-    require('autoprefixer'),
     ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
 }
