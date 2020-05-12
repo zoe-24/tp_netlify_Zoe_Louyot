b79ab2961d5b215cdb42928fbb64defee6535095
Ryan Scherler <ryan@eastslope.studio>
2020-05-12 16:03:03 -0700

    Add optional viewBox param for SVG icon shortcode.

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -1,6 +1,7 @@
 module.exports = {
-  icon: function (name, classes) {
-    return `<svg class="${classes}" role="img" aria-hidden="true">
+  icon: function (name, classes, viewBox) {
+    const viewBoxAttr = viewBox ? `viewBox="${viewBox}"` : ''
+    return `<svg ${viewBoxAttr} class="${classes}" role="img" aria-hidden="true">
                 <use xlink:href="#icon-${name}"></use>
             </svg>`
   },
