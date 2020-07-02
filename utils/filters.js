fd683345389d308b2ea98222dd180888496c75fe
Ryan Thaut <rthaut@gmail.com>
2020-07-01 20:18:30 -0600

    make time zone configurable for date filters

--- utils/filters.js
+++ utils/filters.js
@@ -1,12 +1,24 @@
 const { DateTime } = require('luxon')
 
+const build = require('../src/data/build')
+
+let defaultZone = 'local';
+if (Object.prototype.hasOwnProperty.call(build, 'timezone')) {
+  const testDate = DateTime.local().setZone(build.timezone);
+  if (testDate.isValid) {
+    defaultZone = build.timezone;
+  } else {
+    console.warn(testDate.invalidExplanation);
+  }
+}
+
 module.exports = {
-  dateToFormat: function (date, format) {
-    return DateTime.fromJSDate(date, { zone: 'utc' }).toFormat(String(format))
+  dateToFormat: function (date, format, zone = defaultZone) {
+    return DateTime.fromJSDate(date, { zone }).toFormat(String(format))
   },
 
-  dateToISO: function (date) {
-    return DateTime.fromJSDate(date, { zone: 'utc' }).toISO({
+  dateToISO: function (date, zone = defaultZone) {
+    return DateTime.fromJSDate(date, { zone }).toISO({
       includeOffset: false,
       suppressMilliseconds: true,
     })
